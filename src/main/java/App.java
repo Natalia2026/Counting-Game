6ce48545ccf74b82be47b3af352b7dad760aef56
Natalia2026 <natalia.c.weston@gmail.com>
2026-03-03 11:31:05 +0000

    Added ability to play again

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -18,41 +19,59 @@ public class App {
         return guessInt;
     }
 
-    public static void main(String[] args) {
+    public static void main() {
         //Initialise variables
         int currentGuess = 0;
         boolean winRound = false;
-        int winGame = 0;
+        int winGameCount = 0;
+        boolean winGame = false;
 
-        for (int j=0; j<3; j++){
-            // reset variables
-            currentGuess = 0;
-            winRound = false;
-            // generate number
-            int randomNum = ThreadLocalRandom.current().nextInt(1, 51);
-            System.out.println("Number has been generated.");
-            // 4 guesses
-            for (int i=0;i<4;i++){
-                currentGuess = checkIsInt();
-                if (currentGuess == randomNum){
-                    winRound = true;
-                    break;
-                } else if (currentGuess > randomNum && i<3) {
-                    System.out.println("Incorrect! My number is lower than "+ currentGuess + ".");
-                } else if (currentGuess < randomNum && i<3){
-                    System.out.println("Incorrect! My number is higher than "+ currentGuess + ".");
+        for (int j = 0; j < 3; j++) {
+            if (winGameCount >= 2) {
+                winGame = true;
+                break;
+            } else {
+                // reset variables
+                currentGuess = 0;
+                winRound = false;
+                // generate number
+                int randomNum = ThreadLocalRandom.current().nextInt(1, 51);
+                System.out.println("Number has been generated.");
+                // 4 guesses
+                for (int i = 0; i < 4; i++) {
+                    currentGuess = checkIsInt();
+                    if (currentGuess == randomNum) {
+                        winRound = true;
+                        break;
+                    } else if (currentGuess > randomNum && i < 3) {
+                        System.out.println("Incorrect! My number is lower than " + currentGuess + ".");
+                    } else if (currentGuess < randomNum && i < 3) {
+                        System.out.println("Incorrect! My number is higher than " + currentGuess + ".");
+                    }
                 }
+
+                //Print win or lose message at the end of the round.
+                if (winRound) {
+                    System.out.println("You win this round!");
+                    winGameCount++;
+                } else {
+                    System.out.println("Incorrect! You've run out of guesses. You lose this round!");
+                }
+
+
             }
+        }
+        if (winGame){
+            System.out.println("Congratulations! You won! Would you like to play again?");
+        } else {
+            System.out.println("LOSER! Would you like to play again?");
+        }
 
-            //Print win or lose message at the end of the round.
-            if (winRound){
-                System.out.println("Congratulations! You win this round! ");
-                winGame ++;
-            }else {
-                System.out.println("Incorrect! You've run out of guesses. You lose this round!");
-            }
-
-
+        Scanner s2 = new Scanner(System.in);
+        System.out.println("Please enter Y or N");
+        String playAgain = s2.nextLine();
+        if (Objects.equals(playAgain, "Y")){
+            main();
         }
 
     }
